dd2c2bdde9471bf62f1bc3831efeb4e92f89adc9
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2017-05-23 15:34:35 +0000

    Main accomodation with changes in data_prc and build_net files and build_net.. undergoing trials

--- main/utils/build_network.py
+++ main/utils/build_network.py
@@ -1,11 +1,13 @@
-import numpy as np
 import keras
+from keras import regularizers
 from keras.models import Sequential
 from keras.layers import LSTM
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+#from keras.layers.advanced_activations import PReLU
 
 NUM_CLASSES = 7
+PICTURE_DIM = 48
 
 def print_metadata(x_train, x_test):
     print('x_train shape:', x_train.shape)
@@ -18,22 +20,29 @@ def build_model(x_train):
 
     model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
     model.add(Activation('relu'))
-    model.add(Conv2D(32, (3, 3)))
+    #model.add(PReLU(alpha_initializer='zeros'))
+    
+    
+    model.add(Flatten())
+    
+    model.add(Dense(NUM_CLASSES))
+    model.add(Activation('softmax'))
+
+    return model
+    
+"""model.add(Conv2D(32, (3, 3)))
     model.add(Activation('relu'))
+    
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    #model.add(Dropout(0.25))
 
     model.add(Conv2D(64, (3, 3), padding='same'))
     model.add(Activation('relu'))
     model.add(Conv2D(64, (3, 3)))
     model.add(Activation('relu'))
+    
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    #model.add(Dropout(0.25))
 
     model.add(Flatten())
+    
     model.add(Dense(512))
-    model.add(Activation('relu'))
-    model.add(Dense(NUM_CLASSES))
-    model.add(Activation('softmax'))
-
-    return model
+    model.add(Activation('relu'))"""
