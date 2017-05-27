d6135ba061cfdd92a90db1670852faef39fd8e45
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2017-05-27 14:56:39 +0000

    Conv net with 3 layers and maxpooling

--- main/utils/build_network.py
+++ main/utils/build_network.py
@@ -4,7 +4,6 @@ from keras.models import Sequential
 from keras.layers import LSTM
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-#from keras.layers.advanced_activations import PReLU
 
 NUM_CLASSES = 7
 PICTURE_DIM = 48
@@ -20,29 +19,27 @@ def build_model(x_train):
 
     model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
     model.add(Activation('relu'))
-    #model.add(PReLU(alpha_initializer='zeros'))
+    
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Conv2D(32, (3, 3), activity_regularizer= regularizers.l2(0.00025)))
+    model.add(Activation('relu'))
+    
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Conv2D(64, (3, 3), padding='same', activity_regularizer= regularizers.l2(0.00025)))
+    model.add(Activation('relu'))
     
     
     model.add(Flatten())
+    
+    model.add(Dense(512, activity_regularizer= regularizers.l2(0.00025)))
+    model.add(Activation('relu'))
     
     model.add(Dense(NUM_CLASSES))
     model.add(Activation('softmax'))
 
     return model
     
-"""model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(64, (3, 3), padding='same'))
-    model.add(Activation('relu'))
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Flatten())
-    
-    model.add(Dense(512))
-    model.add(Activation('relu'))"""
+""" model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu')) """
